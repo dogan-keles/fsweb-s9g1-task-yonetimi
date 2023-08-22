f0210e256e5e4848aecaf0f267b0fe6478fc2ea4
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2023-08-22 19:36:03 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import TaskForm from "./TaskForm";
 import TaskHookForm from "./TaskHookForm";
 import PeopleForm from "./PeopleForm";
 import { initialTasks, initialTeam } from "./data";
-
+import { useForm } from "react-hook-form";
 
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   const [team, setTeam] = useState(initialTeam);
-
+  const { register, handleSubmit } = useForm();
   function handleTaskSubmit(yeniTask) {
-    setTasks([yeniTask, ...tasks])
+    setTasks([yeniTask, ...tasks]);
   }
 
   function handlePeopleSubmit(yeniKisi) {
-    setTeam([...team, yeniKisi])
+    setTeam([...team, yeniKisi]);
   }
 
   function handleComplete(id) {
-    console.log("tamamlama fonksiyonunu buraya yazın")
+    console.log("tamamlama fonksiyonunu buraya yazın");
   }
 
   return (
@@ -59,7 +59,6 @@ function App() {
           </div>
         </div>
       </div>
-
     </div>
   );
 }
